81da23c334fa115816708adb7696990cb5817935
sam <sam@Spectre>
2019-01-30 20:31:26 +0000

    Intro phase - hello world

---
+++ gui.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
+
+class MyWindow(Gtk.Window):
+
+    def __init__(self):
+        Gtk.Window.__init__(self, title="Hello World")
+
+        self.button = Gtk.Button(label="Who loves Charlie?")
+        self.button.connect("clicked", self.on_button_clicked)
+        self.add(self.button)
+
+    def on_button_clicked(self, widget):
+        print("I love  Charlie")
+
+win = MyWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
