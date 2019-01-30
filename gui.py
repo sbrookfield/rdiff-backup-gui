514ca41facc08638197885f395813749327823f9
sam <sam@Spectre>
2019-01-30 21:07:33 +0000

    Added functional glade template

--- gui.py
+++ gui.py
@@ -4,19 +4,17 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
-class MyWindow(Gtk.Window):
 
+class gui:
     def __init__(self):
-        Gtk.Window.__init__(self, title="Hello World")
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("gtk/main.glade")
+        self.builder.connect_signals(self)
+        self.window = self.builder.get_object("MainWindow")
+        self.window.show_all()
+    def onDestroy(self, *args):
+        Gtk.main_quit()
 
-        self.button = Gtk.Button(label="Who loves Charlie?")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
-
-    def on_button_clicked(self, widget):
-        print("I love  Charlie")
-
-win = MyWindow()
-win.connect("destroy", Gtk.main_quit)
-win.show_all()
-Gtk.main()
+if __name__ == "__main__":
+    GUI = gui()
+    Gtk.main()
