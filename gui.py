1b8992a06e464c5b4eed344ddbc7588b061a2222
sam <sam@Spectre>
2019-02-03 15:14:28 +0000

    Initial improvements - still not functioning

--- gui.py
+++ gui.py
@@ -11,7 +11,17 @@ class gui:
         self.builder.add_from_file("gtk/main.glade")
         self.builder.connect_signals(self)
         self.window = self.builder.get_object("MainWindow")
-        self.window.show_all()
+        self.header = self.builder.get_object("header")
+        if self.window != None or self.header != None:
+            self.window.set_icon_from_file('128ico.png')
+            self.header.get_parent().remove(self.header)
+            self.window.set_titlebar(self.header)
+            print(self.window.set_default_icon_from_file('128ico.png'))
+            self.window.show_all()
+        else:
+            warn('No window and/or header found in glade file')
+        
+                
     def onDestroy(self, *args):
         Gtk.main_quit()
 
